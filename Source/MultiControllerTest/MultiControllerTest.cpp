80a4b44bc3d98225456bd9a1f6e5b3305c5ed3e0
ogamita777 <ogamita777@gmail.com>
2025-04-27 02:01:05 +0900

    init commit

---
+++ Source/MultiControllerTest/MultiControllerTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiControllerTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiControllerTest, "MultiControllerTest" );
